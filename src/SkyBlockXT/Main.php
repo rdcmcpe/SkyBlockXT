71e5c0135b79ee3f9977465c796d6e2ddc948872
Byron Garza <thekillertacos10@gmail.com>
2016-04-09 20:44:47 -0500

    Update Main.php

--- src/SkyBlockXT/Main.php
+++ src/SkyBlockXT/Main.php
@@ -16,23 +16,22 @@ use pocketmine\level\Level;
 use pocketmine\level\Location;
 use pocketmine\plugin\PluginBase as Base;
 use pocketmine\math\Vector3;
-use pocketmine\block\Dirt;
-use pocketmine\block\Sand;
-use pocketmine\block\Grass;
-use pocketmine\block\Wood;
-use pocketmine\level\generator\object\Tree;
-use pocketmine\tile\Tile;
-use pocketmine\tile\Chest;
-use pocketmine\block\Sapling;
 use pocketmine\utils\Random;
-use SkyBlockXT\utils\SkyBlockGenerator;
+use SkyBlockXT\utils\SkyBlockGenerator; //Will be Replaced Soon
+use SkyBlockXT\Tools\IsleGen;
+
+
+
+//WIP:
+//use SkyBlockXT\Tools\Skyland;
+
+//use SkyBlockXT\Tools\;
+
+//use SkyBlockXT\Tools\AntiTroll;
 
 class Main extends Base implements Listener{
 	public function onEnable(){
 
-		//custom generator
-		Generator::addGenerator ( SkyBlockGenerator::class, "skyblock" );
-		
 		if(!(is_dir($this->getDataFolder().""))){ //would it crash?
 			@mkdir($this->getDataFolder()."");
 		}
@@ -46,6 +45,17 @@ class Main extends Base implements Listener{
 		}
 		$this->getLogger()->info(TextFormat::GREEN . "Done!");
 	}
+	
+	public function onLoad(){
+	  
+	  //In here will be only be used for Plugin Functions, not Plugins data or Related like onEnabled
+	  
+		//custom generator
+		Generator::addGenerator ( SkyBlockGenerator::class, "skyblock" );
+	  $this->makeIsland = new IsleGen($this);
+	  
+	}
+	
 	public function onDisable(){
 		$this->getLogger()->info(TextFormat::RED . "SkyBlock disabled! The Server Crashed or did it stop? [TKRT]");
 		$this->getLogger()->info(TextFormat::GOLD . "or just a little reload?                              [TKRT]");
@@ -84,24 +94,29 @@ class Main extends Base implements Listener{
 							return true;
 							
 						}else{
+						  
 							if(!(file_exists($senderIs))){
 								$this->makeIsland($sender->getName());
 								return true;
 							}else{
+							  
 								$sender->sendMessage(TextFormat::YELLOW . "You already have an island");
 								return true;
 							}
 						}
+						
 					}else{
 						$sender->sendMessage(TextFormat::RED . "You cannot create an island");
 						return true;
 					}
+					
 				}elseif($args[0] == "home"){
 					if($sender->hasPermission("is") || $sender->hasPermission("is.command") || $sender->hasPermission("is.command.home")){
 						if(!(file_exists($this->getDataFolder()."Islands/".$sender->getName().".txt"))){
 							$sender->sendMessage("You don't have an island. Use /is create to make one");
 							return true;
 						}else{
+						  
 							$level = $this->getServer()->getLevelByName(yaml_parse_file($this->getDataFolder()."Players/".$sender->getName().".txt"));
 							if($level !== null){
 								$sender->sendMessage(TextFormat::GREEN."Teleporting to your island...");
@@ -113,15 +128,18 @@ class Main extends Base implements Listener{
 									$sender->sendMessage(TextFormat::GREEN."Done!");
 									return true;
 								}
+								
 							}else{
-								$sender->sendMessage("An error has occored.");
+								$sender->sendMessage(TextFormat::RED . "An error has occored.");
 								return true;
 							}
 						}
+						
 					}else{
-						$sender->sendMessage("You do not have permission to do that");
+						$sender->sendMessage(TextFormat::RED . "You do not have permission to do that");
 						return true;
 					}
+					
 				}elseif($args[0] == "find"){
 					if($sender->hasPermission("is") || $sender->hasPermission("is.command") || $sender->hasPermission("is.command.find")){
 						if(isset($args[1])){
@@ -151,6 +169,7 @@ class Main extends Base implements Listener{
 						$sender->sendMessage(TextFormat::YELLOW . "You cannot find the coords of a player's island");
 						return true;
 					}
+					
 				}elseif($args[0] == "delete"){
 					if($sender->hasPermission("is") || $sender->hasPermission("is.command") || $sender->hasPermission("is.command.delete")){
 						if(!(isset($args[1]))){
@@ -175,6 +194,7 @@ class Main extends Base implements Listener{
 							$sender->sendMessage("You cannot delete your island");
 							return true;
 						}
+						
 					}elseif($args[0] == "sethome"){
 						if($sender->hasPermission("is") || $sender->hasPermission("is.command") || $sender->hasPermission("is.command.sethome")){
 							if(!(isset($args[1]))){
@@ -210,250 +230,7 @@ class Main extends Base implements Listener{
 				}
 			}
 		}
-	
-	public function makeIsland($name){ //Soon to be changed!
-		$player = $this->getServer()->getPlayer($name);
-		if(!($player instanceof Player)){
-			return "Error: Player not found";
-		}else{
-			
-			// Make a file for the island
-			$islandFile = fopen($this->getDataFolder()."Islands/".$name.".txt", "w");
-			fwrite($islandFile, $x.", ".$Y.", ".$z);
-			$playerFile = fopen($this->getDataFolder()."Players/".$name.".txt", "w");
-			fwrite($playerFile, $player->getLevel()->getName());
-			
-			// Top layer of the island
-			// <Callers:
-			//$x = rand(1, 800);
-			//$z = rand(1, 800);
-			$z = 100;
-			$x = 100;
-			$Y = 75;
-			$id= 2;
-			$idsand = 12;
-			$iddirt = 3;
-			$idwood = 17;
-			$idleaves = 18;
-			$idchest = 54;
-			$sender = $this->getServer()->getPlayer($name)
-			// End of Callers>
-			
-			// 1st side
-			$sender->getLevel()->setBlockIdAt($x, $Y, $z, $id);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y, $z+6, $id);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y, $z+5, $id);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y, $z+4, $id);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y, $z+3, $id);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y, $z+2, $id);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y, $z+1, $id);
-			
-			// 2nd side
-			$sender->getLevel()->setBlockIdAt($x+5, $Y, $z+6, $id);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y, $z+5, $id);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y, $z+4, $id);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y, $z+3, $id);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y, $z+2, $id);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y, $z+1, $id);
-			
-			// 3rd side 
-			$sender->getLevel()->setBlockIdAt($x+4, $Y, $z+6, $id);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y, $z+5, $id);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y, $z+4, $id);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y, $z+3, $id);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y, $z+2, $id);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y, $z+1, $id);
-			
-			// 4th side
-			$sender->getLevel()->setBlockIdAt($x+3, $Y, $z+6, $id);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y, $z+5, $id);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y, $z+4, $id);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y, $z+3, $id);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y, $z+2, $id);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y, $z+1, $id);
-			
-			// 5th side
-			$sender->getLevel()->setBlockIdAt($x+2, $Y, $z+6, $id);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y, $z+5, $id);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y, $z+4, $id);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y, $z+3, $id);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y, $z+2, $id);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y, $z+1, $id);
-			
-			// 6th side
-			$sender->getLevel()->setBlockIdAt($x+1, $Y, $z+6, $id);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y, $z+5, $id);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y, $z+4, $id);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y, $z+3, $id);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y, $z+2, $id);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y, $z+1, $id);
-			
-			// Middle layer of the island
-			
-			// 1st side
-			$sender->getLevel()->setBlockIdAt($x, $Y-1, $z, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y-1, $z+6, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y-1, $z+5, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y-1, $z+4, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y-1, $z+3, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y-1, $z+2, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y-1, $z+1, $idsand);
-			
-			// 2nd side
-			$sender->getLevel()->setBlockIdAt($x+5, $Y-1, $z+6, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y-1, $z+5, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y-1, $z+4, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y-1, $z+3, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y-1, $z+2, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y-1, $z+1, $idsand);
-			
-			// 3rd side
-			$sender->getLevel()->setBlockIdAt($x+4, $Y-1, $z+6, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y-1, $z+5, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y-1, $z+4, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y-1, $z+3, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y-1, $z+2, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y-1, $z+1, $idsand);
-			
-			// 4th side
-			$sender->getLevel()->setBlockIdAt($x+3, $Y-1, $z+6, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y-1, $z+5, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y-1, $z+4, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y-1, $z+3, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y-1, $z+2, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y-1, $z+1, $idsand);
-			
-			// 5th side
-			$sender->getLevel()->setBlockIdAt($x+2, $Y-1, $z+6, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y-1, $z+5, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y-1, $z+4, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y-1, $z+3, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y-1, $z+2, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y-1, $z+1, $idsand);
-			
-			// 6th side
-			$sender->getLevel()->setBlockIdAt($x+1, $Y-1, $z+6, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y-1, $z+5, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y-1, $z+4, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y-1, $z+3, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y-1, $z+2, $idsand);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y-1, $z+1, $idsand);
-			
-			
-			
-			// Bottom layer of the island
-			
-			// 1st side
-			$sender->getLevel()->setBlockIdAt($x, $Y-2, $z, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y-2, $z+6, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y-2, $z+5, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y-2, $z+4, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y-2, $z+3, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y-2, $z+2, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+6, $Y-2, $z+1, $iddirt);
-			
-			// 2nd side
-			$sender->getLevel()->setBlockIdAt($x+5, $Y-2, $z+6, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y-2, $z+5, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y-2, $z+4, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y-2, $z+3, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y-2, $z+2, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+5, $Y-2, $z+1, $iddirt);
-			
-			// 3rd side
-			$sender->getLevel()->setBlockIdAt($x+4, $Y-2, $z+6, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y-2, $z+5, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y-2, $z+4, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y-2, $z+3, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y-2, $z+2, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+4, $Y-2, $z+1, $iddirt);
-			
-			// 4th side
-			$sender->getLevel()->setBlockIdAt($x+3, $Y-2, $z+6, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y-2, $z+5, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y-2, $z+4, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y-2, $z+3, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y-2, $z+2, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+3, $Y-2, $z+1, $iddirt);
-			
-			// 5th side
-			$sender->getLevel()->setBlockIdAt($x+2, $Y-2, $z+6, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y-2, $z+5, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y-2, $z+4, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y-2, $z+3, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y-2, $z+2, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+2, $Y-2, $z+1, $iddirt);
-			
-			// 6th side
-			$sender->getLevel()->setBlockIdAt($x+1, $Y-2, $z+6, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y-2, $z+5, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y-2, $z+4, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y-2, $z+3, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y-2, $z+2, $iddirt);
-			$sender->getLevel()->setBlockIdAt($x+1, $Y-2, $z+1, $iddirt);
-			
-			
-			// Teleport the player to their new island
-			$player->teleport(new Position($x, $Y+5, $z, $this->getServer()->getLevelByName($levelName)));
-			$player->sendMessage(TextFormat::GREEN . "Welcome to your new island");
-			$player->sendMessage(TextFormat::GREEN . "If your island didn't spawn,");
-			$player->sendMessage(TextFormat::GREEN . "Use /is delete");
-			$player->sendMessage(TextFormat::GREEN . "Then make a new island");
-			
-			// Give the player a starter kit
-			
-			// String
-			$player->getInventory()->addItem(Item::get(287));
-			$player->getInventory()->addItem(Item::get(287));
-			$player->getInventory()->addItem(Item::get(287));
-			$player->getInventory()->addItem(Item::get(287));
-			$player->getInventory()->addItem(Item::get(287));
-			
-			// Emerald
-			$player->getInventory()->addItem(Item::get(388));
-			$player->getInventory()->addItem(Item::get(388));
-			$player->getInventory()->addItem(Item::get(388));
-			$player->getInventory()->addItem(Item::get(388));
-			$player->getInventory()->addItem(Item::get(388));
-			
-			// Sapling
-			$player->getInventory()->addItem(Item::get(6));
-			$player->getInventory()->addItem(Item::get(6));
-			$player->getInventory()->addItem(Item::get(6));
-			$player->getInventory()->addItem(Item::get(6));
-			$player->getInventory()->addItem(Item::get(6));
-			
-			// Water
-			$player->getInventory()->addItem(Item::get(8));
-			$player->getInventory()->addItem(Item::get(8));
-			
-			// Lava
-			$player->getInventory()->addItem(Item::get(10));
-			
-			// Seeds
-			$player->getInventory()->addItem(Item::get(295));
-			$player->getInventory()->addItem(Item::get(295));
-			$player->getInventory()->addItem(Item::get(295));
-			$player->getInventory()->addItem(Item::get(295));
-			$player->getInventory()->addItem(Item::get(295));
-			
-			// Melon seeds
-			$player->getInventory()->addItem(Item::get(362));
-			
-			// Cactus
-			$player->getInventory()->addItem(Item::get(81));
-			
-			// Iron
-			$player->getInventory()->addItem(Item::get(265));
-			$player->getInventory()->addItem(Item::get(265));
-			$player->getInventory()->addItem(Item::get(265));
-			$player->getInventory()->addItem(Item::get(265));
-			$player->getInventory()->addItem(Item::get(265));
-			$player->getInventory()->addItem(Item::get(265));
-			
-			$this->getLogger()->info($name . TextFormat::YELLOW . " made an island!");
-		}
-	}
+
 	
 	// When a player joins
 	public function onPlayerJoinEvent(PlayerJoinEvent $event){
